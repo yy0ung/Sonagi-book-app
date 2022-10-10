e7a855a87447b0883b399c18f12a9225ca2808a2
Young <syoh0313@yonsei.ac.kr>
2022-10-11 00:56:02 +0900

    design : calendar

--- app/src/main/java/young/com/sonagibook_app/ScheduleFragment.kt
+++ app/src/main/java/young/com/sonagibook_app/ScheduleFragment.kt
@@ -9,22 +9,35 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.CalendarView
 import android.widget.ImageView
 import androidx.lifecycle.ViewModelProvider
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView
+import com.prolificinteractive.materialcalendarview.format.ArrayWeekDayFormatter
+import com.prolificinteractive.materialcalendarview.format.MonthArrayTitleFormatter
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import java.time.DayOfWeek
+import java.util.*
 
 class ScheduleFragment : Fragment() {
     private lateinit var viewModelFactory: MainViewModelFactory
     private lateinit var viewModel: MainViewModel
+    lateinit var calendarView : MaterialCalendarView
+
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_schedule, container, false)
         val addBtn : ImageView = view.findViewById(R.id.scheduleAddBtn)
+
+        calendarView = view.findViewById(R.id.scheduleCalender)
+
+        setCalendar(calendarView)
 
         viewModel = ViewModelProvider(requireActivity(), MainViewModelFactory(Repository())).get(
             MainViewModel::class.java)
@@ -51,6 +64,14 @@ class ScheduleFragment : Fragment() {
         withContext(Dispatchers.IO){
             while (viewModel.homeScheduleDataModel.size==0){}
         }
+    }
+
+    private fun setCalendar(cal : MaterialCalendarView){
+
+        cal.setTitleFormatter(MonthArrayTitleFormatter(resources.getStringArray(R.array.custom_months)))
+        cal.setWeekDayFormatter(ArrayWeekDayFormatter(resources.getStringArray(R.array.custom_weekdays)))
+
+
     }
 
 }
